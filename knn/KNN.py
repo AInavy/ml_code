9ae33d6a4b60be4ffd87b9765c549a232815238d
AInavy <navyfm@163.com>
2019-05-30 20:14:55 +0800

    knn

--- knn/KNN.py
+++ knn/KNN.py
@@ -1,4 +1,5 @@
 #! -*- coding=utf-8 -*-
+from os import listdir
 from numpy import *
 import operator
 
@@ -29,8 +30,8 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(),
                               key=operator.itemgetter(1), reverse=True)
 
-    for i in range(0, len(sortedClassCount)):
-        print sortedClassCount[i]
+    # for i in range(0, len(sortedClassCount)):
+    #     print sortedClassCount[i]
 
     return sortedClassCount[0][0]
 
@@ -62,3 +63,55 @@ def autoNorm(dataSet):
     normDataSet = normDataSet / tile(ranges, (m, 1))
 
     return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('./datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+
+    for i in range(numTestVecs):
+        # normMat[i, :] 单一预测样本;  normMat[numTestVecs:m, :] 900训练样本
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]) :
+            errorCount += 1.0
+        print "the total error rate is: %f " % (errorCount / float(numTestVecs))
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games? "))
+    ffMiles = float(raw_input("frequent flier miles earned per year? "))
+    iceCream = float(raw_input("liters of ice cream consumed per year? "))
+
+    datingDataMat, datingLabels = file2matrix('./datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = [percentTats, ffMiles, iceCream]
+
+    classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+
+    print "You will probably like this person: ", resultList[classifierResult - 1]
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        linestr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(linestr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('./digits/trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        # print fileNameStr   # txtfilename
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
--- knn/main.py
+++ knn/main.py
@@ -7,25 +7,44 @@ import matplotlib.pyplot as plt
 import KNN
 
 if __name__ == "__main__":
-    print "1--"
-    group, labels = KNN.createDataSet()
-    print group
-    # print type(group)
-    print labels
+    # print "1--"
+    # group, labels = KNN.createDataSet()
+    # print group
+    # # print type(group)
+    # print labels
+    #
+    # print KNN.classify0([0,0], group, labels, 3)
+    #
+    # print "2--"
+    # datingDataMat, datingLabels = KNN.file2matrix('./datingTestSet2.txt')
+    # # print datingDataMat
+    # # print datingLabels
+    #
+    # # show viewer
+    # fig = plt.figure()
+    # plt.title('knn')
+    # plt.xlabel('X')
+    # plt.ylabel('Y')
+    #
+    # ax1 = fig.add_subplot(211)
+    # ax1.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
+    #            15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    #
+    # ax2 = fig.add_subplot(212)
+    # ax2.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+    # plt.show()
+    #
+    # normMat, ranges, minVals = KNN.autoNorm(datingDataMat)
+    # print minVals, ranges
+    # print normMat
 
-    print KNN.classify0([0,0], group, labels, 3)
+    # KNN.datingClassTest()
+    # KNN.classifyPerson()
+    # testVector = KNN.img2vector('./digits/testDigits/0_13.txt')
+    # print testVector[0, 0:31]
+    # print testVector[0, 32:63]
 
-    print "2--"
-    datingDataMat, datingLabels = KNN.file2matrix('./datingTestSet2.txt')
-    # print datingDataMat
-    # print datingLabels
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
-               15.0 * array(datingLabels), 15.0 * array(datingLabels))
-    plt.show()
-
-    normMat, ranges, minVals = KNN.autoNorm(datingDataMat)
-    print minVals, ranges
-    print normMat
-
+    # KNN.handwritingClassTest()
+    arr = [1,2,3,4,5]
+    arr_mean = mean(arr)
+    print arr_mean
